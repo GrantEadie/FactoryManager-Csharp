693b6f6a8cb41d7df29ea63fc3073bab47b75eee
Grant Eadie <grantleadie@gmail.com>
2020-10-17 09:09:50 -0700

    continue engineer view build.

--- FactoryManager/Controllers/MachinesController.cs
+++ FactoryManager/Controllers/MachinesController.cs
@@ -28,97 +28,97 @@ namespace FactoryManager.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Machine mission)
+    public ActionResult Create(Machine machine)
     {
-      _db.Machines.Add(mission);
+      _db.Machines.Add(machine);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }  
     public ActionResult Details(int id)
     {
       var thisMachine = _db.Machines
-        .Include(mission => mission.Engineers)
+        .Include(machine => machine.Engineers)
         .ThenInclude(join => join.Engineer)
-        .Include(mission => mission.Incidents)
+        .Include(machine => machine.Incidents)
         .ThenInclude(join => join.Incident)
-        .FirstOrDefault(mission => mission.MachineId == id);
+        .FirstOrDefault(machine => machine.MachineId == id);
       return View(thisMachine);
     }
     public ActionResult Edit(int id)
     {
-      var thisMachine = _db.Machines.FirstOrDefault(missions => missions.MachineId == id);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
       ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
       return View(thisMachine);
     }
 
     [HttpPost]
-    public ActionResult Edit(Machine mission)
+    public ActionResult Edit(Machine machine)
     {
-      _db.Entry(mission).State = EntityState.Modified;
+      _db.Entry(machine).State = EntityState.Modified;
       _db.SaveChanges();
-      return RedirectToAction("Details", new { id = mission.MachineId });
+      return RedirectToAction("Details", new { id = machine.MachineId });
     }
     public ActionResult Delete(int id)
     {
-      var thisMachine = _db.Machines.FirstOrDefault(missions => missions.MachineId == id);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
       return View(thisMachine);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisMachine = _db.Machines.FirstOrDefault(missions => missions.MachineId == id);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
       _db.Machines.Remove(thisMachine);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
     [HttpPost]
-    public ActionResult DeleteEngineer(int joinId, int missionId)
+    public ActionResult DeleteEngineer(int joinId, int machineId)
     {
       var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
       _db.EngineerMachine.Remove(joinEntry);
       _db.SaveChanges();
-      return RedirectToAction("Details", new {id = missionId});
+      return RedirectToAction("Details", new {id = machineId});
     }
     public ActionResult AddEngineer(int id)
     {
-      var thisMachine = _db.Machines.FirstOrDefault(missions => missions.MachineId == id);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
       ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
       return View(thisMachine);
     }
     [HttpPost]
-    public ActionResult AddEngineer(Machine mission, int EngineerId)
+    public ActionResult AddEngineer(Machine machine, int EngineerId)
     {
       if (EngineerId != 0)
       {
-      _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = mission.MachineId });
+      _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
       }
       _db.SaveChanges();
-      return RedirectToAction("Details", new { id = mission.MachineId});
+      return RedirectToAction("Details", new { id = machine.MachineId});
     }
     [HttpPost]
-    public ActionResult DeleteIncident(int joinId, int missionId)
+    public ActionResult DeleteIncident(int joinId, int machineId)
     {
       var joinEntry = _db.MachineIncident.FirstOrDefault(entry => entry.MachineIncidentId == joinId);
       _db.MachineIncident.Remove(joinEntry);
       _db.SaveChanges();
-      return RedirectToAction("Details", new {id = missionId});
+      return RedirectToAction("Details", new {id = machineId});
     }
     public ActionResult AddIncident(int id)
     {
-      var thisMachine = _db.Machines.FirstOrDefault(missions => missions.MachineId == id);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
       ViewBag.IncidentId = new SelectList(_db.Incidents, "IncidentId", "IncidentTitle");
       return View(thisMachine);
     }
     [HttpPost]
-    public ActionResult AddIncident(Machine mission, int IncidentId)
+    public ActionResult AddIncident(Machine machine, int IncidentId)
     {
       if (IncidentId != 0)
       {
-      _db.MachineIncident.Add(new MachineIncident() { IncidentId = IncidentId, MachineId = mission.MachineId });
+      _db.MachineIncident.Add(new MachineIncident() { IncidentId = IncidentId, MachineId = machine.MachineId });
       }
       _db.SaveChanges();
-      return RedirectToAction("Details", new { id = mission.MachineId});
+      return RedirectToAction("Details", new { id = machine.MachineId});
     }
   }
 }
